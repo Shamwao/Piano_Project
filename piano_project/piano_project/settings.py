9402979662e433bf9fb3ce71649cb2d6887ae9f6
jdawg <manofivory@gmail.com>
2023-07-14 11:10:08 -0600

    static files now serve in both modes

--- piano_project/piano_project/settings.py
+++ piano_project/piano_project/settings.py
@@ -143,7 +143,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
-# STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_ROOT = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
