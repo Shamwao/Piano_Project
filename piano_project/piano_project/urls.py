813b44ac78c727d7ef4ec4e804811fe2cdd29458
jdawg <manofivory@gmail.com>
2023-07-14 11:10:23 -0600

    static file config

--- piano_project/piano_project/urls.py
+++ piano_project/piano_project/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,6 @@ urlpatterns = [
     path('tutorial/', include('tutorial_app.urls')),
     path('profile/', include('profile_app.urls')),
     path('lessons/', include('lessons_app.urls'))
-]
+] 
+if settings.DEBUG: 
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
