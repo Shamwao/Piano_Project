6711b20d915604d994d26494598f3d772af8306e
jdawg <manofivory@gmail.com>
2023-07-19 15:13:55 -0600

    fixed bug that allowed continuous clicking

--- piano_project/static/js/quizzy.js
+++ piano_project/static/js/quizzy.js
@@ -2,7 +2,6 @@
 
 const testKeys = ["A","B","C","D","E","F","G"]
 const answerKeys = document.getElementsByClassName('white-keys')
-// const answerKeys = querySelectorAll('.white-keys', '.black-keys')
 let score = 0
 let finalScore= 0
 let passed = false
@@ -20,66 +19,65 @@ function poseQuestion(){
             return realAnswer
 }
 
-document.querySelectorAll('.useranswer').forEach((el) =>{
-    el.addEventListener("click", function(){
-        checkAnswer(this.id)
-    })
-})
-
-function checkAnswer(id){
-    userAnswer = id
-    msg = ''
-    console.log("Real Answer:" , realAnswer)
-    console.log("User said:", userAnswer)
-    if (realAnswer == userAnswer){
-        console.log("Nailed it")
-        questionsAnswered += 1
-        score += 10
-        document.getElementById('score').innerText = "Score: "+ score
-        poseQuestion()
-    }
-        else {
-        console.log("Not this time")
-        questionsAnswered += 1
-        document.getElementById('score').innerText = "Score: "+ score
-        poseQuestion()
-    }
-    if (score == 100){
-        msg = ". You passed!"
-    }
-    else {
-        msg = ". Keep training."
-    }
-    if (questionsAnswered == 10){
-        quizComplete = true
-        finalScore = score
-        alert("Your final score is " + finalScore+ msg)
-        document.getElementById('finalScore').value = finalScore
-        console.log(score)
-        if (finalScore == 100){
-            passed = true
+document.querySelectorAll('.useranswer').forEach((el) => {
+    el.addEventListener("click", function () {
+        if (!quizComplete) {
+        checkAnswer(this.id);
         }
-        else {
-            passed = false
-        }
-        document.getElementById('passed').value = passed
-        console.log(passed)
-        clean()
-        document.getElementById('complete').removeAttribute('hidden')
-    }
-}
+    });
+});
 
-function hide(element){
-    element.remove()
+function checkAnswer(id) {
+    userAnswer = id;
+    msg = '';
+    console.log("Real Answer:", realAnswer);
+    console.log("User said:", userAnswer);
+    if (realAnswer == userAnswer) {
+        console.log("Nailed it");
+        questionsAnswered += 1;
+        score += 10;
+        document.getElementById('score').innerText = "Score: " + score;
+        clean();
+        poseQuestion();
+    } else {
+        console.log("Not this time");
+        questionsAnswered += 1;
+        document.getElementById('score').innerText = "Score: " + score;
+        clean();
+        poseQuestion();
+    }
+
+    if (questionsAnswered == 10) {
+        quizComplete = true;
+        finalScore = score;
+        alert("Your final score is " + finalScore + msg);
+        document.getElementById('finalScore').value = finalScore;
+        console.log(score);
+
+        if (finalScore == 100) {
+        passed = true;
+        } else {
+        passed = false;
+        }
+
+        document.getElementById('passed').value = passed;
+        console.log(passed);
+        clean();
+        document.getElementById('complete').removeAttribute('hidden');
+
+        document.querySelectorAll('.useranswer').forEach((el) => {
+        el.removeEventListener("click", checkAnswer);
+        });
+    }
 }
 
 function clean() {
-    for (var i = 0; i < answerKeys.length; i++)
-        answerKeys[i].style.backgroundColor = 'white'
+    for (var i = 0; i < answerKeys.length; i++) {
+        answerKeys[i].style.backgroundColor = 'white';
+    }
 }
-
-
-
-
+function hide(element){
+    element.remove()
+}
 
 //quiz/scales
