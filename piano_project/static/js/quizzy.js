dfd59fa75323b6b710651d55f078a52dadd12d67
jdawg <manofivory@gmail.com>
2023-07-19 15:59:46 -0600

    cleaned up...still weird backend bug though

--- piano_project/static/js/quizzy.js
+++ piano_project/static/js/quizzy.js
@@ -1,81 +1,69 @@
 //quiz/keys 
 
-const testKeys = ["A","B","C","D","E","F","G"]
-const answerKeys = document.getElementsByClassName('white-keys')
-let score = 0
-let finalScore= 0
-let passed = false
-let questionsAnswered = 0
-let quizComplete = false
+    const testKeys = ["A", "B", "C", "D", "E", "F", "G"];
+    const answerKeys = document.getElementsByClassName('white-keys');
+    let score = 0;
+    let questionsAnswered = 0;
+    let quizComplete = false;
 
-function poseQuestion(){
-    let random = Math.floor(Math.random()*testKeys.length)
-    console.log("randomly selected key:", testKeys[random])
-    for (var i = 0; i <answerKeys.length; i ++)
-        if (answerKeys[i].id == testKeys[random])
-            var answer=answerKeys[i]
-            answer.style.backgroundColor = 'red';
-            realAnswer = answer.id
-            return realAnswer
-}
+    function poseQuestion() {
+      let random = Math.floor(Math.random() * testKeys.length);
+        console.log("randomly selected key:", testKeys[random]);
+        const randomAnswer = answerKeys[random];
+        randomAnswer.style.backgroundColor = 'red';
+        realAnswer = randomAnswer.id;
+        return realAnswer
+    }
 
-document.querySelectorAll('.useranswer').forEach((el) => {
-    el.addEventListener("click", function () {
+    document.querySelectorAll('.useranswer').forEach((el) => {
+        el.addEventListener("click", function () {
         if (!quizComplete) {
-        checkAnswer(this.id);
+            checkAnswer(this.id);
         }
-    });
-});
-
-function checkAnswer(id) {
-    userAnswer = id;
-    msg = '';
-    console.log("Real Answer:", realAnswer);
-    console.log("User said:", userAnswer);
-    if (realAnswer == userAnswer) {
-        console.log("Nailed it");
-        questionsAnswered += 1;
-        score += 10;
-        document.getElementById('score').innerText = "Score: " + score;
-        clean();
-        poseQuestion();
-    } else {
-        console.log("Not this time");
-        questionsAnswered += 1;
-        document.getElementById('score').innerText = "Score: " + score;
-        clean();
-        poseQuestion();
-    }
-
-    if (questionsAnswered == 10) {
-        quizComplete = true;
-        finalScore = score;
-        alert("Your final score is " + finalScore + msg);
-        document.getElementById('finalScore').value = finalScore;
-        console.log(score);
-
-        if (finalScore == 100) {
-        passed = true;
-        } else {
-        passed = false;
-        }
-
-        document.getElementById('passed').value = passed;
-        console.log(passed);
-        clean();
-        document.getElementById('complete').removeAttribute('hidden');
-
-        document.querySelectorAll('.useranswer').forEach((el) => {
-        el.removeEventListener("click", checkAnswer);
         });
-    }
-}
+    });
 
-function clean() {
-    for (var i = 0; i < answerKeys.length; i++) {
-        answerKeys[i].style.backgroundColor = 'white';
+    function checkAnswer(id) {
+        const userAnswer = id;
+        console.log("Real Answer:", realAnswer);
+        console.log("User said:", userAnswer);
+        if (realAnswer == userAnswer) {
+            console.log("Nailed it");
+            questionsAnswered++;
+            score += 10;
+            document.getElementById('score').innerText = "Score: " + score;
+            clean();
+            poseQuestion();
+        } else {
+            console.log("Not this time");
+            questionsAnswered++;
+            document.getElementById('score').innerText = "Score: " + score;
+            clean();
+            poseQuestion();
+        }
+
+        if (questionsAnswered == 10) {
+            quizComplete = true;
+            const finalScore = score;
+            const passed = finalScore === 100;
+            // Display the final score and pass/fail status in the UI
+            document.getElementById('finalScore').value = finalScore;
+            document.getElementById('passed').value = passed;
+            clean();
+            document.getElementById('complete').removeAttribute('hidden');
+        
+        document.querySelectorAll('.useranswer').forEach((el) => {
+            el.removeEventListener("click", checkAnswer);
+        });
+        }
     }
-}
+
+    function clean() {
+        for (let i = 0; i < answerKeys.length; i++) {
+            answerKeys[i].style.backgroundColor = 'white';
+        } 
+    }
+
 function hide(element){
     element.remove()
 }
