0e48910fb485e4f3e89462dd3bd0ed1c83e72ec1
jdawg <manofivory@gmail.com>
2023-07-27 14:02:38 -0600

    add config to templates

--- piano_project/piano_project/settings.py
+++ piano_project/piano_project/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
+                'django.template.context_processors.static',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
