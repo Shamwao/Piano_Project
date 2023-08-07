f2e9be522d0108a23a1b3acfd16decb8b44f3787
jdawg <manofivory@gmail.com>
2023-08-07 17:35:47 -0600

    updated bkey quiz to function like wkey

--- piano_project/static/js/quizzy2.js
+++ piano_project/static/js/quizzy2.js
@@ -1,81 +1,77 @@
 //quiz/keys 
 
 const testKeys = ["Db","Eb","Gb","Ab","Bb"]
-const answerKeys = document.getElementsByClassName('black-keys')
-// const answerKeys = querySelectorAll('.white-keys', '.black-keys')
-let score = 0
-let finalScore= 0
-let passed = false
-let questionsAnswered = 0
-let quizComplete = false
+const answerKeys = document.getElementsByClassName('black-keys');
+let score = 0;
+let questionsAnswered = 0;
+let quizComplete = false;
 
-function poseQuestion(){
-    let random = Math.floor(Math.random()*testKeys.length)
-    console.log("randomly selected key:", testKeys[random])
-    for (var i = 0; i <answerKeys.length; i ++)
-        if (answerKeys[i].id == testKeys[random])
-            var answer=answerKeys[i]
-            answer.style.backgroundColor = 'red';
-            realAnswer = answer.id
-            return realAnswer
+function poseQuestion() {
+  let random = Math.floor(Math.random() * testKeys.length);
+    const randomAnswer = answerKeys[random];
+    console.log("randomly selected key:", testKeys[random]);
+    randomAnswer.style.backgroundColor = 'red';
+    realAnswer = randomAnswer.id;
+    return realAnswer
 }
 
-document.querySelectorAll('.useranswer').forEach((el) =>{
-    el.addEventListener("click", function(){
-        checkAnswer(this.id)
-    })
-})
+document.querySelectorAll('.useranswer').forEach((el) => {
+    el.addEventListener("click", function () {
+    if (!quizComplete) {
+        checkAnswer(this.id);
+    }
+    });
+});
 
-function checkAnswer(id){
-    userAnswer = id
-    msg = ''
-    console.log("Real Answer:" , realAnswer)
-    console.log("User said:", userAnswer)
-    if (realAnswer == userAnswer){
-        console.log("Nailed it")
-        questionsAnswered += 1
-        score += 10
-        document.getElementById('score').innerText = "Score: "+ score
-        poseQuestion()
+function checkAnswer(id) {
+    const userAnswer = id;
+    console.log("Real Answer:", realAnswer);
+    console.log("User said:", userAnswer);
+    if (realAnswer == userAnswer) {
+        console.log("Nailed it");
+        questionsAnswered++;
+        score += 10;
+        document.getElementById('score').innerText = "Score: " + score;
+        clean();
+        poseQuestion();
+    } else {
+        console.log("Not this time");
+        questionsAnswered++;
+        document.getElementById('score').innerText = "Score: " + score;
+        clean();
+        poseQuestion();
     }
-        else {
-        console.log("Not this time")
-        questionsAnswered += 1
-        document.getElementById('score').innerText = "Score: "+ score
-        poseQuestion()
-    }
-    if (score == 100){
-        msg = ". You passed!"
-    }
-    else {
-        msg = ". Keep training."
-    }
-    if (questionsAnswered == 10){
-        quizComplete = true
-        finalScore = score
-        alert("Your final score is " + finalScore+ msg)
-        document.getElementById('finalScore').value = finalScore
-        console.log(score)
+
+    if (questionsAnswered == 10) {
+        quizComplete = true;
+        const finalScore = score;
+        const passed = finalScore === 100;
         if (finalScore == 100){
-            passed = true
+            document.getElementById('top-text').innerText = 'You scored: ' + finalScore + ". Congrats, you passed!"
+        } else {
+            document.getElementById('top-text').innerText = 'You scored: ' + finalScore + ". Keep practicing!"
         }
-        else {
-            passed = false
-        }
-        document.getElementById('passed').value = passed
-        console.log(passed)
-        clean()
-        document.getElementById('complete').removeAttribute('hidden')
-    }
-}
+        document.getElementById('finalScore').value = finalScore;
+        document.getElementById('passed').value = passed;
+        clean();
+        document.getElementById('finish').removeAttribute('hidden');
+        document.getElementById('retake').removeAttribute('hidden');
 
-function hide(element){
-    element.remove()
+    
+    document.querySelectorAll('.useranswer').forEach((el) => {
+        el.removeEventListener("click", checkAnswer);
+    });
+    }
 }
 
 function clean() {
-    for (var i = 0; i < answerKeys.length; i++)
-        answerKeys[i].style.backgroundColor = 'black'
+    for (let i = 0; i < answerKeys.length; i++) {
+        answerKeys[i].style.backgroundColor = 'black';
+    } 
+}
+
+function hide(element){
+element.remove()
 }
 
 
